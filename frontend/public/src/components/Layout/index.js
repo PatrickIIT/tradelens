2f3a139c3b728859d207f311e098107821477156
PatrickIIT <zda24m007@iitmz.ac.in>
2026-05-21 16:22:07 +0300

    Create index.js

---
+++ frontend/public/src/components/Layout/index.js
@@ -0,0 +1,4 @@
+// frontend/src/components/Layout/index.js
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as Footer } from './Footer';
